c91eb10b171279a9109c208835b5e56d0f413a16
Kaypix <127738552+Kaypix@users.noreply.github.com>
2025-03-12 22:11:13 +0100

    Update fcts.h

--- fcts.h
+++ fcts.h
@@ -1,8 +1,10 @@
 #ifndef FCTS_H
 
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 
 Eigen::VectorXf dydt(float t, Eigen::VectorXf y) ;
+
+Eigen::VectorXf dydt_lin(float t, Eigen::VectorXf y) ;
 
 Eigen::VectorXf sol(float t, Eigen::VectorXf y_0) ;
 
