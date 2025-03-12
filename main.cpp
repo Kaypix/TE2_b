710c4f752a74e575ce8bd58eca26f7c98e741582
Kaypix <127738552+Kaypix@users.noreply.github.com>
2025-03-12 23:14:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ extern float k ;
 
 int main () {
     k=1 ;
-    float t0 = 0 ; float xmax = 50 ; int p = 2 ; 
+    float t0 = 0 ; float xmax = 50 ; int p = 2 ; int m=1 ; 
     float h ; 
     string nom_titre, nom_fichier, pas ;
     int i, choix ;
@@ -41,11 +41,13 @@ int main () {
     MatrixXf delta(p,size1) ;
     
     estime.col(0)=y_0;
+    float E0 = 0.5 * (estime(1,0))*(estime(1,0)) ;
 
     for (i = 0; i < size1; ++i) {
         t(i) = t0 + i * h ;
         exact.col(i) = sol(t(i), y_0) ;
     }
+    
     delta.col(0) = exact.col(0) - estime.col(0) ;
 #include <bits/stdc++.h> 
 
@@ -55,22 +57,30 @@ int main () {
             nom_fichier="eulerprog" ;
             estime.col(i) = eulerProg (dydt, estime.col(i-1) , t(i-1), h) ;
             delta.col(i) = exact.col(i) - estime.col(i) ;
+            float Em = (0.5 * estime(1,i) * estime(1,i)) + (k * k * (1 - cos(estime(0,i)))) ; //(0.5 * m * v * v) + (0.5 * k *  y *  y) ;
+            float deltaE = (Em-E0)/E0 ;
+            cout << "L'energie mécanique est : " << Em << " l'écart relatif est " << deltaE << endl ;
         }
         if (choix == 2) {
             nom_titre="Point du milieu" ;
             nom_fichier="pointMilieu" ;
             estime.col(i) = pointMilieu (dydt, estime.col(i-1) , t(i-1), h) ;
             delta.col(i) = exact.col(i) - estime.col(i) ;
-
+            float Em = (0.5 * estime(1,i) * estime(1,i)) + (k * k * (1 - cos(estime(0,i)))) ; //(0.5 * m * v * v) + (0.5 * k *  y *  y) ;
+            float deltaE = (Em-E0)/E0 ;
+            cout << "L'energie mécanique est : " << Em << " l'écart relatif est " << deltaE << endl ;
         }
         if (choix==3) {
             nom_titre="Runge kutta d'ordre 4" ;
             nom_fichier="rk4" ;
             estime.col(i) = rk4 (dydt, estime.col(i-1) , t(i-1), h) ;
             delta.col(i) = exact.col(i) - estime.col(i) ;
+            float Em = (0.5 * estime(1,i) * estime(1,i)) + (k * k * (1 - cos(estime(0,i)))) ; //(0.5 * m * v * v) + (0.5 * k *  y *  y) ;
+            float deltaE = (Em-E0)/E0 ;
+            cout << "L'energie mécanique est : " << Em << " l'écart relatif est " << deltaE << endl ;
         }      
     }
-
+    
     pas=to_string(h); 
     ecrit(nom_fichier + "_" + pas + ".txt", t, exact, estime, delta, nom_titre, h) ;
     cout << "Résultats écrits dans le fichier txt correspondant." << endl;
