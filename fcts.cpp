398e1595a1142ddb778709cd12687a14198d513e
Kaypix <127738552+Kaypix@users.noreply.github.com>
2025-03-12 22:10:58 +0100

    Update fcts.cpp

--- fcts.cpp
+++ fcts.cpp
@@ -1,7 +1,7 @@
 #include "fcts.h"
 #include <cmath>
 #include <iostream>
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 
 using namespace std ;
 using namespace Eigen ;
@@ -11,13 +11,20 @@ float k;
 VectorXf dydt(float t, VectorXf y) {
     VectorXf dydt(y.size()); // p = dimension de y(t)
     dydt(0) = y(1);
-    dydt(1) = -sin(y(0));
+    dydt(1) = -k*k*sin(y(0));
     return dydt ;
 }
 
+//Fonction second membre dydt pour le système linéaire du pendule
+VectorXf dydt_lin(float t, VectorXf y) {
+    VectorXf dy(2);
+    dy(0) = y(1);            // v = y'
+    dy(1) = -k * k * y(0);   // v' = -k^2 y
+    return dy ;
+}
 VectorXf sol(float t, VectorXf y_0) {
     VectorXf solAna(2) ;
-    solAna(0) = y_0(0)*cos(k*t) + (y_0(1)/k*sin(k*t)) ;
+    solAna(0) = y_0(0)*cos(k*t) + ((y_0(1)/k)*sin(k*t)) ;
     solAna(1) = y_0(0)*k*sin(k*t) + y_0(1)*cos(k*t) ;
     return solAna ;
 }
