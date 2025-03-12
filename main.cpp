a70314530638cd636f3776fe1fb1fc2da841f997
Kaypix <127738552+Kaypix@users.noreply.github.com>
2025-03-12 22:10:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 #include <cmath>
 #include <bits/stdc++.h> 
 
@@ -15,7 +15,7 @@ extern float k ;
 
 int main () {
     k=1 ;
-    float t0 = 0 ; float xmax = 20 ; int p = 2 ; 
+    float t0 = 0 ; float xmax = 50 ; int p = 2 ; 
     float h ; 
     string nom_titre, nom_fichier, pas ;
     int i, choix ;
@@ -34,38 +34,44 @@ int main () {
     int size1 = ((xmax - t0) / h) + 1;
 
     VectorXf y_0(p) ;
-    y_0(0)=0 ; y_0(1)=0.2 ;
+    y_0(0)=0 ; y_0(1)=1.98 ;
     VectorXf t(size1) ;
     MatrixXf exact(p, size1) ;
     MatrixXf estime(p, size1) ;
+    MatrixXf delta(p,size1) ;
+    
+    estime.col(0)=y_0;
 
     for (i = 0; i < size1; ++i) {
         t(i) = t0 + i * h ;
         exact.col(i) = sol(t(i), y_0) ;
     }
-
+    delta.col(0) = exact.col(0) - estime.col(0) ;
 #include <bits/stdc++.h> 
 
-    for (i = 0 ; i< size1 ; ++i){
+    for (i = 1 ; i< size1 ; ++i){
         if (choix == 1) {
             nom_titre="Euler progressive" ;
             nom_fichier="eulerprog" ;
-            estime.col(i+1) = eulerProg (dydt, estime.col(i) , t(i), h) ;
+            estime.col(i) = eulerProg (dydt, estime.col(i-1) , t(i-1), h) ;
+            delta.col(i) = exact.col(i) - estime.col(i) ;
         }
         if (choix == 2) {
             nom_titre="Point du milieu" ;
             nom_fichier="pointMilieu" ;
-            estime.col(i+1) = pointMilieu (dydt, estime.col(i) , t(i), h) ; //estime.col(i+1) = milieu(estime.col(i), t(i), h, dydt);
+            estime.col(i) = pointMilieu (dydt, estime.col(i-1) , t(i-1), h) ;
+            delta.col(i) = exact.col(i) - estime.col(i) ;
 
         }
         if (choix==3) {
             nom_titre="Runge kutta d'ordre 4" ;
             nom_fichier="rk4" ;
-            estime.col(i+1) = rk4 (dydt, estime.col(i) , t(i), h) ;
+            estime.col(i) = rk4 (dydt, estime.col(i-1) , t(i-1), h) ;
+            delta.col(i) = exact.col(i) - estime.col(i) ;
         }      
     }
 
     pas=to_string(h); 
-    ecrit(nom_fichier + "_" + pas + ".txt", t, exact, estime, nom_titre, h) ;
+    ecrit(nom_fichier + "_" + pas + ".txt", t, exact, estime, delta, nom_titre, h) ;
     cout << "Résultats écrits dans le fichier txt correspondant." << endl;
 }
