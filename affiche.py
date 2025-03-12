bf36169302d69f2c41165f9a7c61225a9c5178ad
Kaypix <127738552+Kaypix@users.noreply.github.com>
2025-03-12 22:14:50 +0100

    Update affiche.py

--- affiche.py
+++ affiche.py
@@ -2,71 +2,167 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys 
 
+#---------- Position h=0.1 ----------
 
-# Variable a modifier avant execution
+tab = np.loadtxt("eulerprog_0.100000.txt", skiprows=3)
+t = tab[:,0]
+pos_exact = tab[:,1]
+plt.plot(t, pos_exact, label="Position théorique")
 
-list_fichier = ["eulerprog_0.200000.txt","eulerprog_0.100000.txt", "eulerprog_0.010000.txt", "pointMilieu_0.200000.txt", "pointMilieu_0.100000.txt", "pointMilieu_0.010000.txt", "rk4_0.200000.txt", "rk4_0.100000.txt", "rk4_0.010000.txt"]
-list_legend = ["eulerprog0.2","eulerprog0.1", "eulerprog0.01", "pointMilieu0.2", "pointMilieu0.1", "pointMilieu0.01", "rk40.2", "rk40.1", "rk40.01"]
-for fichier in list_fichier:
-  tab = np.loadtxt(fichier)
-  x = tab[:,0]
-  estime = tab[:,2]
-  err = tab[:,3]
-  plt.plot(x, estime, label="Observée")
-  print(fichier, "Le max de l'erreur est :", np.max(err))
+pos_estime1 = tab[:,3]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, pos_estime1, label="Position Euler")  
 
-tab_th = np.loadtxt("eulerprog_0.100000.txt")
-exact = tab_th[:,1]
-x_th = tab_th[:,0]
-plt.plot(x_th, exact, label="Théorique")   
+tab = np.loadtxt("pointMilieu_0.100000.txt", skiprows=3)
+t = tab[:,0]
+pos_estime2 = tab[:,3]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, pos_estime2, label="Position Point Milieu")  
 
-for legend in list_legend :
-  plt.legend(legend)
+tab = np.loadtxt("rk4_0.100000.txt", skiprows=3)
+t = tab[:,0]
+pos_estime3 = tab[:,3]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, pos_estime3, label="Position RK4")  
 
-#Erreur max : (5.17) pour 0.1 et (9.33) pour 0.2 pour la méthode d'euler
-
-# Creation du graphique
-
-#plt.title( titre, fontsize=8)
+plt.legend(prop={'size': 7}) 
+plt.title("Positions avec h=0.1", fontsize=8)
 plt.grid()
 
-plt.xlabel("x")                               # <== variable d'abscisse
-plt.ylabel("y")                                   # <== variable d'ordonnee
+plt.xlabel("t")                               # <== variable d'abscisse
+plt.ylabel("positions")                                   # <== variable d'ordonnee
 
-plt.savefig("fig.pdf") # sauvegarde du fichier.
-plt.show()
-plt.clf() # nettoyage ('clear figure')
-
-'''
-plt.plot(x,err, label="Erreur entre les deux solutions")
-plt.xlabel("x")                                   # <== variable d'abscisse
-plt.ylabel("y")                                   # <== variable d'ordonnee
-
-plt.legend()
+plt.savefig("positions0.1.pdf") # sauvegarde du fichier.
 plt.show()
 
-#Lire l'entête
+#---------- Vitesse h=0.1 ----------
 
-# ou bien indiquer le nom du fichier sur la ligne de commande
-if len(sys.argv) > 1:
-    nomfichier = sys.argv[1]
+tab = np.loadtxt("eulerprog_0.100000.txt", skiprows=3)
+t = tab[:,0]
+vit_exact = tab[:,2]
+plt.plot(t, vit_exact, label="Vitesse théorique")
 
-try:      # ouverture du fichier de donnees
-  fich  = open(nomfichier, "r")
-except:
-  print(" ! Impossible d'ouvrir le fichier %s."%nomfichier )
-  print(" ! Verifier le chemin.\n")
-  exit()
+vit_estime1 = tab[:,4]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, vit_estime1, label="Vitesse Euler")  
 
-# Lecture des lignes d'entete
-ligne = fich.readline().replace("\n","") # sans caractere de fin de ligne
-entete= [ ]
-while( ligne[0] == "#" ):
-   entete.append( ligne[1:] )
-   ligne =  fich.readline().replace("\n","")
-fich.close()
-print( " > fichier %s : %d lignes d'entete"%( nomfichier, len(entete) ) )
+tab = np.loadtxt("pointMilieu_0.100000.txt", skiprows=3)
+t = tab[:,0]
+vit_estime2 = tab[:,4]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, vit_estime2, label="Vitesse Point Milieu")  
 
-# concatenation des entetes en une seule ligne de titre :
-titre = " ".join(entete)
-'''
+tab = np.loadtxt("rk4_0.100000.txt", skiprows=3)
+t = tab[:,0]
+vit_estime3 = tab[:,4]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, vit_estime3, label="Vitesse RK4")  
+
+plt.legend(prop={'size': 7}) 
+plt.title("Vitesses avec h=0.1", fontsize=8)
+plt.grid()
+
+plt.xlabel("t")                              
+plt.ylabel("vitesse")                                   
+
+plt.savefig("vitesse0.1.pdf") # sauvegarde du fichier.
+plt.show()
+
+#---------- Vitesses en fonction de position 0.1 ----------
+
+plt.plot(pos_estime1, vit_estime1, label="Euler")
+plt.plot(pos_estime2, vit_estime2, label="Point Milieu")
+plt.plot(pos_estime3, vit_estime3, label="RK4")
+plt.legend(prop={'size': 7}) 
+plt.title("Vitesses en fonction de la position pour h=0.1", fontsize=8)
+plt.grid()
+
+plt.xlabel("position")                               
+plt.ylabel("vitesse")                                  
+
+plt.savefig("vit(pos)0.1.pdf")
+plt.show()
+
+#---------- Position h=0.01 ----------
+
+tab = np.loadtxt("eulerprog_0.010000.txt", skiprows=3)
+t = tab[:,0]
+pos_exact = tab[:,1]
+plt.plot(t, pos_exact, label="Position théorique")
+pos_estime1 = tab[:,3]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, pos_estime1, label="Position Euler")  
+
+tab = np.loadtxt("pointMilieu_0.010000.txt", skiprows=3)
+t = tab[:,0]
+pos_exact = tab[:,1]
+plt.plot(t, pos_exact, label="Position théorique")
+pos_estime2 = tab[:,3]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, pos_estime2, label="Position Point Milieu")  
+
+tab = np.loadtxt("rk4_0.010000.txt", skiprows=3)
+t = tab[:,0]
+pos_exact = tab[:,1]
+plt.plot(t, pos_exact, label="Position théorique")
+pos_estime3 = tab[:,3]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, pos_estime3, label="Position RK4")  
+
+plt.legend(prop={'size': 7}) 
+plt.title("Positions avec h=0.01", fontsize=8)
+plt.grid()
+
+plt.xlabel("t")
+plt.ylabel("position")                                   
+
+plt.savefig("position0.01.pdf") # sauvegarde du fichier.
+plt.show()
+
+#---------- Vitesse h=0.01 ----------
+
+tab = np.loadtxt("eulerprog_0.010000.txt", skiprows=3)
+t = tab[:,0]
+vit_exact = tab[:,2]
+plt.plot(t, pos_exact, label="Vitesse théorique")
+
+vit_estime1 = tab[:,4]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, vit_estime1, label="Vitesse Euler")  
+
+tab = np.loadtxt("pointMilieu_0.010000.txt", skiprows=3)
+t = tab[:,0]
+vit_exact = tab[:,2]
+plt.plot(t, pos_exact, label="Vitesse théorique")
+vit_estime2 = tab[:,4]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, vit_estime2, label="Vitesse Point Milieu")  
+
+tab = np.loadtxt("rk4_0.010000.txt", skiprows=3)
+t = tab[:,0]
+vit_exact = tab[:,2]
+plt.plot(t, pos_exact, label="Vitesse théorique")
+vit_estime3 = tab[:,4]
+#pos_err = tab[:,5]
+#vit_err = tab[:,6]
+plt.plot(t, vit_estime3, label="Vitesse RK4")  
+
+plt.legend(prop={'size': 7}) 
+plt.title("Vitesses avec h=0.01", fontsize=8)
+plt.grid()
+
+plt.xlabel("t")                               
+plt.ylabel("vitesse")                                   
+
+plt.savefig("vitesse0.01.pdf") # sauvegarde du fichier.
