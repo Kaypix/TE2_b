b2578bee401d36352a091a3103996bbe96c95e91
Kaypix <127738552+Kaypix@users.noreply.github.com>
2025-03-12 22:11:38 +0100

    Update util.cpp

--- util.cpp
+++ util.cpp
@@ -1,30 +1,25 @@
 #include "util.h"
 #include <fstream>
-#include <eigen3/Eigen/Dense>
+#include <Eigen/Dense>
 #include <iomanip>
 #include <limits>
 
 using namespace std ;
 using namespace Eigen ;
 
-void ecrit(const string& nomFichier, const VectorXf& t, const MatrixXf& exact, const MatrixXf& estime, string nom, const float h) {
+void ecrit(const string& nomFichier, const VectorXf& t, const MatrixXf& exact, const MatrixXf& estime, const MatrixXf& delta, string nom, const float h) {
     ofstream fichier(nomFichier);
     fichier << fixed << setprecision(7);
-/*
-    //Calcul des valeurs extrêmes des abscisses et ordonnées
-    float x_min = x.minCoeff();
-    float x_max = x.maxCoeff();
-    float y_min = y.minCoeff();
-    float y_max = y.maxCoeff();
-*/
+
     //Écriture de l'entête avec les valeurs extrêmes
     fichier << "# Pas : h=  " << h << "\n";
     fichier << "# Méthode : " << nom << "\n";
     fichier << "# \n";
 
     for (size_t i = 0; i < t.size(); ++i) {
-        fichier << t(i) << "\t" << exact(0,i) << "\t" << exact(1,i) << "\t" << estime(0,i) << "\t" << estime(1,i) << "\t" << exact.col(i) - estime.col(i)<< "\n";
+        fichier << t(i) << "\t" << exact(0,i) << "\t" << exact(1,i) << "\t" << estime(0,i) << "\t" << estime(1,i) <<"\t" << delta(0,i) << "\t" << delta(1,i) << endl ;
     }
 
     fichier.close() ;
 }
+
